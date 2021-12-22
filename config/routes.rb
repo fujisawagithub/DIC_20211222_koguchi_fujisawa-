f5a2f1c53da7eba047d4c9682c42acef209387bc
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-22 17:21:08 +0900

    ログイン機能実装

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  root to: 'users#new'
+  resources :blogs
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
+end
